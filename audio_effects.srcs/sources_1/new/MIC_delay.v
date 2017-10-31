96f64a1b2e79531e63c00e51d292eeea552a037e
johnweikangong <ongwk95@hotmail.com>
2017-10-31 17:16:35 +0800

    MIC_delay: Add different delay timings

--- audio_effects.srcs/sources_1/new/MIC_delay.v
+++ audio_effects.srcs/sources_1/new/MIC_delay.v
@@ -21,31 +21,50 @@
 
 
 module MIC_delay(
-    input MIC_IN_DELAY_SWITCH,
+    input [3:0] MIC_IN_DELAY_SWITCH,
     input CLK,
     input [11:0] MIC_IN,
     output [11:0] MIC_IN_DELAY,
     output [7:0] MIC_IN_DELAY_LED
     );
     
-    wire clk_20k;
-    reg [12:0] i;
-    reg N;
-    reg [11:0] memory [0:4999];
+    wire clk_16k; // 0.25 second delay
+    wire clk_8k; // 0.5 second delay
+    wire clk_4k; // 1 second delay
+    wire clk_2k; // 2 seconds delay
+    reg slw_clk; // Assigned frequency
     
-    clk_divider slw_clk_20k(CLK, 2499, clk_20k);
+    reg [12:0] i;
+    reg [11:0] memory [0:3999];
+    
+    clk_divider slw_clk_16k(CLK, 3124, clk_16k);
+    clk_divider slk_clk_8k(CLK, 6249, clk_8k);
+    clk_divider slk_clk_4k(CLK, 12499, clk_4k);
+    clk_divider slw_clk_2k(CLK, 24999, clk_2k);
+    
+    // Assign frequency based on MIC_IN_DELAY_SWITCH
+    always @* begin
+        case(MIC_IN_DELAY_SWITCH)
+            1: begin slw_clk = clk_16k; end
+            2: begin slw_clk = clk_8k; end
+            4: begin slw_clk = clk_4k; end
+            8: begin slw_clk = clk_2k; end
+            default: begin slw_clk = 0; end
+        endcase
+    end
     
     // Storing mic data into memory array
-    always @ (posedge clk_20k) begin
-        if(MIC_IN_DELAY_SWITCH) begin
+    always @ (posedge slw_clk) begin
+        if(MIC_IN_DELAY_SWITCH == 1 || MIC_IN_DELAY_SWITCH == 2
+        || MIC_IN_DELAY_SWITCH == 4 || MIC_IN_DELAY_SWITCH == 8) begin
             memory[0] = MIC_IN;
-            for (i = 0; i < 4999; i = i+1) begin
+            for (i = 0; i < 3999; i = i+1) begin
                 memory[i+1] <= memory[i];
             end
         end
     end
     
-    assign MIC_IN_DELAY = (MIC_IN_DELAY_SWITCH == 0) ? 0 : memory[4999];
+    assign MIC_IN_DELAY = (MIC_IN_DELAY_SWITCH == 0) ? 0 : memory[3999];
     assign MIC_IN_DELAY_LED = (MIC_IN_DELAY_SWITCH == 0)? 0 : 8'b10000000;
     
 endmodule
